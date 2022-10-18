3adbfc9dfb4b5610a0cbf0e1c94559a3e4fb3aa8
MataiP <MayanZTC@gmail.com>
2022-10-17 20:23:39 -0700

    Simple Patterns
    Random Rectangles monochromatic

--- Week4/sketch.js
+++ Week4/sketch.js
@@ -1,44 +1,39 @@
-let gridStep = 50;
-let radius = 20;
-let velX, velY = 2;
+size = 30;
+widthMult = 10;
+heightMult = 2;
 
 function setup() {
-  createCanvas(800, 600);
-  colorMode(HSB, width, height, 100);
-  background(30, 80, 80);
-
-}
-
-function draw() {
-  background(0);
-  
-  
-  //showGrid();
-  noStroke();
-  for (let x = 0; x < width / gridStep; x++) {
-    for (let y = 0; y < height / gridStep; y++) {
-      //console.log(x, y);
-      let posX = (x * gridStep) + (gridStep * 0.5);
-      let posY = (y * gridStep) + (gridStep * 0.5);
-      
-      //posX += random(-5, 5);
-      //posY += raondom(-5, 5);
-      
-      
-      fill(posX, posY, 180);
-      circle(posX, posY, radius * 2);
+  createCanvas(700, 700);
+  angleMode(DEGREES);
+  strokeWeight(3);
+  //noStroke();
+  startR = random(165);
+  startG = random(165);
+  startB = random(165);
+  background(startR + 45, startG + 45, startB + 45);
+  for (x = width; x > -size * widthMult; x -= size) {
+    for (y = height; y > -size * heightMult; y -= size) {
+      fill(
+        random(startR, startR + 90),
+        random(startG, startG + 90),
+        random(startB, startB + 90)
+      );
+      push();
+      translate(x + size / 2, y + size / 2);
+      rotate(floor(random(8))*45); 
+      rect(
+        0,
+        0,
+        size * floor(random(1, widthMult)),
+        size * floor(random(1, heightMult))
+      );
+      pop();
     }
   }
 }
 
-function showGrid() {
-  stroke(0);
-  for (let x = 0; x < width; x = x + gridStep) {
-    line(x, 0, x, height);
-  }
-
-  stroke(270, 100, 100);
-  for (let y = 0; y < height; y += gridStep) {
-    line(0, y, width, y);
+function keyTyped() {
+  if (key === "s") {
+    save("myCanvas.jpg");
   }
 }
